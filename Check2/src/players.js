71b3a2656c1d5301bc431a2626de71292923cd18
Tarekjalali <jlali.tare9@gmail.com>
2024-08-29 17:08:54 +0100

    check props 2

---
+++ Check2/src/players.js
@@ -0,0 +1,29 @@
+var players = [
+    {
+        "name":"Neymar",
+        "team":"Alhilal",
+        "nationality":"Barzil",
+        "Number":10,
+        "age":32,
+        "url":"https://b.fssta.com/uploads/application/soccer/headshots/713.png"
+    },
+    {
+        "name":"Iniesta",
+        "team":"Emirates",
+        "nationality":"Spanish",
+        "Number":8,
+        "age":40,
+        "url":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Andr%C3%A9s_Iniesta_%28cropped%29.jpg/330px-Andr%C3%A9s_Iniesta_%28cropped%29.jpg"
+    },
+    {
+        "name":"Slouma",
+        "team":"Mestir",
+        "nationality":"Tunisian",
+        "Number":1,
+        "age":35,
+        "url":"https://www.footballdatabase.eu/images/photos/players/a_295/295615.jpg"
+    },
+]
+
+
+export default players
